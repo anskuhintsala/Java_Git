0b526b2a365a5636b6dbef5a7722374001c6e965
Annastiina Hintsala <anskuhintsala@gmail.com>
2025-10-23 19:33:16 +0300

    Poistettiin tekstiä.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,49 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        
+    int age = 50;
+
+//Tarkistetaan, että ikä ei ole negatiivinen
+
+
+
+if (age > 0 && age < 17) {
+
+System.out.println("Olet alaikäinen");
+if (age >= 16) {
+    System.out.println("Saat ajaa mopoa");
+if (age >= 16) {
+    System.out.println("Saat ajaa kevaria");
+
 }
+}
+
+}
+else if (age >=65)
+{
+    System.out.println("Olet eläkeläinen");
+
+
+if(age==65)
+    System.out.println("Hyviä eläkepäiviä");
+}
+if(age>=58)
+    System.out.println("Voit jäädä varhaiseläkkeelle");
+
+if(age >=40 && age <=50) 
+    System.out.println("Hyvää keski-ikää!");
+
+
+if(age == 100) {
+    System.out.println("Onnea 100-vuotiaalle!");
+    System.out.println("Onnea 100-vuotiaalle!");
+    System.out.println("Onnea 100-vuotiaalle!");
+}
+else
+    System.out.println("Olet aikuinen");
+
+if (age == 18) {
+    System.out.println("Olet tullut juuri täysi-ikäiseksi ja saat ajaa autoa");
+
+}
+    }}
